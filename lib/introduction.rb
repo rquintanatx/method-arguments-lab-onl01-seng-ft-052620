0ae99001344a8d6f1f6a85dde195fd6129cbf66b
Robert Quintana <robertquintanatx@gmail.com>
2020-05-27 07:02:29 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hey there, #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Your name is #{name}, and your language of choice is #{language}."
+end
